fa4edee268ae8424f9dda42a95f9e5b4128f8a53
Jeremy Eissler <jeissler0@gmail.com>
2025-10-13 14:14:53 -0500

    replace real api call for demo

--- src/composables/usePricingData.ts
+++ src/composables/usePricingData.ts
@@ -9,18 +9,17 @@ export function usePricingData(): {
   error: Ref<Error | null>
 } {
   async function fetchPricingData(): Promise<PricingData> {
-    await new Promise((resolve) => setTimeout(resolve, 500)) // simulate loading time
+    // await new Promise((resolve) => setTimeout(resolve, 500)) // simulate loading time
+    // return sampleData as PricingData
 
-    // const apiUrl =
-    //   'https://neuron.serifhealth.com/api/rates/v1?network_template_ids=07c56f6b-82cd-44a4-af42-d570b6ae89c6&limit=1000&codes=99203'
-    // const response = await fetch(apiUrl, {
-    //   headers: {
-    //     'X-API-KEY': import.meta.env.VITE_API_KEY,
-    //   },
-    // })
-    // return await response.json()
-
-    return sampleData as PricingData
+    const apiUrl =
+      'https://neuron.serifhealth.com/api/rates/v1?network_template_ids=07c56f6b-82cd-44a4-af42-d570b6ae89c6&limit=1000&codes=99203'
+    const response = await fetch(apiUrl, {
+      headers: {
+        'X-API-KEY': import.meta.env.VITE_API_KEY,
+      },
+    })
+    return await response.json()
   }
 
   const { data, isLoading, error } = useQuery({
