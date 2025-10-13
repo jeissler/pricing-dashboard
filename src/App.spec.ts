3dadf5a4e1ba275ac4a451dc0d25c285d1f816d2
Jeremy Eissler <jeissler0@gmail.com>
2025-10-13 13:18:17 -0500

    fix test

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,11 +1,24 @@
-import { describe, it, expect } from 'vitest'
-
-import { mount } from '@vue/test-utils'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { mount, VueWrapper } from '@vue/test-utils'
+import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 
 describe('App', () => {
+  let wrapper: VueWrapper<InstanceType<typeof App>>
+
+  beforeEach(() => {
+    const testApp = createApp(App)
+    testApp.use(VueQueryPlugin)
+
+    wrapper = mount(App, {
+      global: {
+        plugins: [VueQueryPlugin],
+      },
+    })
+  })
+
   it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+    expect(wrapper.text()).toContain('Pricing Dashboard')
   })
 })
