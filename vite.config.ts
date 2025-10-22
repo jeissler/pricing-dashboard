6cb6917fa2e461486bca72ecd20da4a0fa0067a1
Jeremy Eissler <jeissler0@gmail.com>
2025-10-22 10:47:41 -0500

    add visualizer for vite bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), visualizer({ open: true, gzipSize: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
